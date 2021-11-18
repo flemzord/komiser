4ada3a254a2c9216a4fd1700219f4f761266f33a
Maxence Maireaux <maxence@maireaux.fr>
2021-11-18 13:02:51 +0100

    Add component for Menu

--- dashboard/components/Menu.vue
+++ dashboard/components/Menu.vue
@@ -1,99 +1,16 @@
 <template>
   <nav class="mt-10">
-    <a
-      class="
-        flex
-        items-center
-        mt-4
-        py-2
-        px-6
-        bg-gray-700 bg-opacity-25
-        text-gray-100
-      "
-      href="/"
-    >
-      <font-awesome-icon :icon="['fas', 'house-damage']" />
-      <span class="mx-3">Dashboard</span>
-    </a>
-
-    <a
-      class="
-        flex
-        items-center
-        mt-4
-        py-2
-        px-6
-        text-gray-500
-        hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100
-      "
-      href="/"
-    >
-      <font-awesome-icon :icon="['fas', 'lock']" />
-      <span class="mx-3">Compute</span>
-    </a>
-
-    <a
-      class="
-        flex
-        items-center
-        mt-4
-        py-2
-        px-6
-        text-gray-500
-        hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100
-      "
-      ref="/"
-    >
-      <font-awesome-icon :icon="['fas', 'warehouse']" />
-      <span class="mx-3">Storage</span>
-    </a>
-
-    <a
-      class="
-        flex
-        items-center
-        mt-4
-        py-2
-        px-6
-        text-gray-500
-        hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100
-      "
-      ref="/"
-    >
-      <font-awesome-icon :icon="['fas', 'network-wired']" />
-      <span class="mx-3">Network</span>
-    </a>
-
-    <a
-      class="
-        flex
-        items-center
-        mt-4
-        py-2
-        px-6
-        text-gray-500
-        hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100
-      "
-      ref="/"
-    >
-      <font-awesome-icon :icon="['fas', 'shield-alt']" />
-      <span class="mx-3">Security</span>
-    </a>
-
-    <a
-      class="
-        flex
-        items-center
-        mt-4
-        py-2
-        px-6
-        text-gray-500
-        hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100
-      "
-      ref="/"
-    >
-      <font-awesome-icon :icon="['fas', 'database']" />
-      <span class="mx-3">Data & AI</span>
-    </a>
+    <MenuItem name="Dashboard" url="/" fa="house-damage" />
+    <MenuItem name="Compute" url="/compute" fa="lock" />
+    <MenuItem name="Storage" url="/storage" fa="warehouse" />
+    <MenuItem name="Network" url="/network" fa="network-wired" />
+    <MenuItem name="Security" url="/security" fa="shield-alt" />
+    <MenuItem name="Data & AI" url="/data" fa="database" />
   </nav>
 </template>
+<script>
+import MenuItem from "./MenuItem";
+export default {
+  components: {MenuItem}
+}
+</script>
---
+++ dashboard/components/MenuItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <nuxt-link :to=url class="
+        flex
+        items-center
+        mt-4
+        py-2
+        px-6
+        text-gray-500
+        hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100
+    " >
+    <font-awesome-icon :icon=fa />
+    <span class="mx-3">{{ name }}</span>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: ['name', "url", "fa"]
+}
+</script>
